5106e13630d7ebba3593eab00e37c5be82925bac
Matt Imdad <jjimdad@gmail.com>
2021-02-21 19:57:17 -0600

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Student on 2/21/21.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
